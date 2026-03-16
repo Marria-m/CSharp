ca43de92bb77b50ca9d663f3c7b6530d7db80c21
Mariam Ehab <160272932+Marria-m@users.noreply.github.com>
2026-03-16 20:58:29 +0200

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolSystem.Data.Contexts;
+using SchoolSystem.Models;
+
+namespace SchoolSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        // /Student/ShowAll
+        public IActionResult ShowAll()
+        {
+            StudentBL studentBL = new StudentBL();
+            List<Student> studentList = studentBL.GetAll();
+            return View("ShowAll", studentList); // Model: List<Student>
+        }
+
+        // /Student/ShowDetails?id=3
+        public IActionResult ShowDetails(int id)
+        {
+            StudentBL studentBL = new StudentBL();
+            Student student = studentBL.GetById(id);
+            return View("ShowDetails", student); // Model: Student
+        }
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolSystem.Models
+{
+    // Many-to-One with Course      → CourseId FK
+    // Many-to-One with Department  → DepartmentId FK
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string? Address { get; set; }
+
+        [ForeignKey("Course")]
+        public int? CourseId { get; set; }
+
+        [ForeignKey("Department")]
+        public int? DepartmentId { get; set; }
+
+        public Course? Course { get; set; }
+        public Department? Department { get; set; }
+    }
+}
